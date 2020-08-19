08a388d201f3aa65e871c06becb2f1423a19ea3a
Ayrton Saito <tonsaito@Ayrtons-MacBook-Pro.local>
2020-08-19 17:05:56 -0300

    updating AccessConsumerService to write on a csv file

--- access-consumer/src/main/java/br/com/itau/mastertech/acesso/access/consumer/service/AccessConsumerService.java
+++ access-consumer/src/main/java/br/com/itau/mastertech/acesso/access/consumer/service/AccessConsumerService.java
@@ -5,11 +5,35 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import java.io.*;
+
 @Component
 public class AccessConsumerService {
 
     @KafkaListener(topics = "spec4-ayrton-saito-1", groupId = "default")
     public void readMessage(@Payload AccessModel accessModel) {
-        System.out.println("message received. client: "+accessModel.getClientId()+" | door: "+accessModel.getDoorId()+" | has Access? "+accessModel.getHasAccess());
+         try{
+            FileWriter fileWriter = new FileWriter("logAccess.csv", true);
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+            StringBuilder sb = new StringBuilder();
+            sb.append("Client ID,");
+            sb.append("DOOR ID,");
+            sb.append("HAS ACCESS?");
+            sb.append('\n');
+
+            sb.append(accessModel.getClientId());
+            sb.append(',');
+            sb.append(accessModel.getDoorId());
+            sb.append(',');
+            sb.append(accessModel.getHasAccess());
+            sb.append('\n');
+
+            printWriter.write(sb.toString());
+            printWriter.close();
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
